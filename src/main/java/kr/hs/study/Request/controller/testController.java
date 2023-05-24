a7e5b77113e6015ef223a5755eeb6375161e104a
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-24 12:14:02 +0900

    @PathVariable, @RequestParam 실습

--- src/main/java/kr/hs/study/Request/controller/testController.java
+++ src/main/java/kr/hs/study/Request/controller/testController.java
@@ -2,13 +2,19 @@ package kr.hs.study.Request.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static org.apache.coyote.http11.Constants.a;
+
 // Controller를 써야한단다 ... ^^
 @Controller
 public class testController {
+
     // HttpServletRequest
     @GetMapping("/test1")
     public String test1(HttpServletRequest req) {
@@ -50,6 +56,30 @@ public class testController {
         for(String str : c) {
             System.out.println("data3 : " + str);
         }
+
+        return "result";
+    }
+
+    // @PathVariable
+    @GetMapping("/test4/{a}/{b}/{c}")
+    public String test4(@PathVariable String a,
+                        @PathVariable String b,
+                        @PathVariable String c) {
+        System.out.println("data1 : " + a);
+        System.out.println("data2 : " + b);
+        System.out.println("data3 : " + c);
+
+        return "result";
+    }
+
+    // @RequestParam
+    @GetMapping("/test5")
+    public String test5(@RequestParam("data1") String data1,
+                        @RequestParam("data2") String data2,
+                        @RequestParam("data3") String data3) {
+        System.out.println("data1 : " + data1);
+        System.out.println("data1 : " + data2);
+        System.out.println("data1 : " + data3);
 
         return "result";
     }
